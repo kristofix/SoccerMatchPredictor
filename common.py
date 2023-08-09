53610b92f318dc622e55e994c9cb52308dd43f00
kk <krzysztof.krystek@gmail.com>
2023-08-09 21:36:06 +0200

    s

---
+++ common.py
@@ -0,0 +1,47 @@
+
+def removeDotFromColumnNames(df):#
+    df.columns = df.columns.str.replace('.', '')
+    return df
+
+def dropMinutes(df):#
+    df = df[(df['framestime'] > min) & (df['framestime'] < max)]
+    df.drop(['framestime'], axis=1, inplace=True)
+    return df
+
+def sortByDate(df):#
+    df['datetimestamp'] = pd.to_datetime(df['datetimestamp'], unit='ms')
+    df = df.sort_values(by='datetimestamp')
+    return df
+
+
+def dropNotDraw(df):#
+  #drop mathces without draw in betting frame
+  df['draw'] = df['frameshomescore'] - df['framesawayscore']
+  df = df[df['draw'] == 0]
+  df.drop(['frameshomescore','framesawayscore'], axis=1, inplace=True)
+  #drop 'draw' column
+  df.drop(['draw'], axis=1, inplace=True)
+  return df
+
+def oddsFilter(df):#
+  df = df[(df['frameshomeodd'] >= minbetodd) & (df['frameshomeodd'] <= maxbetodd) & (df['framesawayodd'] >= minbetodd) & (df['framesawayodd'] <= maxbetodd)]
+  return df
+
+def addSumStats(df):#
+  df['sumAstats'] = df['frameshomeshotsOnTarget'] + df['frameshomeshotsOffTarget']+ df['frameshomeattacks']+ df['frameshomedangerousAttacks']
+  df['sumBstats'] = df['framesawayshotsOnTarget'] + df['framesawayshotsOffTarget']+ df['framesawayattacks']+ df['framesawaydangerousAttacks']
+  return df
+
+def dropInsufficient(df):#
+  #drop records with insufficient value of summary data
+  df = df[(df['sumAstats'] >= insufficient) | (df['sumBstats'] >= insufficient)]
+  return df
+
+def dif_thresholdd(df):
+  # Calculate the difference between the values in columns 'sumAstats' and 'sumBstats'
+  df['diff'] = abs(df['sumAstats'] - df['sumBstats'])
+  # Delete rows where the difference is less than dif_threshold
+  df = df[df['diff'] >= dif_threshold]
+  # Drop the 'diff' column, as it is no longer needed
+  df = df.drop(columns='diff')
+  return df
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import lightgbm as lgb
+from lightgbm import LGBMClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+from functools import partial
 import pandas as pd
+from ramda import pipe
+from functools import partial
 import numpy as np
+
+from common import removeDotFromColumnNames, dropMinutes, sortByDate, dropNotDraw, oddsFilter, addSumStats, dropInsufficient, dif_thresholdd,
+
+
+
 df = pd.read_csv('/home/kk/PycharmProjects/oddmaker/data/exp23_withLeagues_LIMITED_minutes_4-35_odd_1.1-5_insfufficient_10_dif_1_onlyDraws.csv')
 print(df.head(5))
+
