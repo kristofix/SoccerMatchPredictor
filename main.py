e6b8ffebf60a199b050867467e43e74993ed6870
kk <krzysztof.krystek@gmail.com>
2023-08-09 21:26:04 +0200

    s

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import pandas as pd
 import numpy as np
-d
+df = pd.read_csv('/home/kk/PycharmProjects/oddmaker/data/exp23_withLeagues_LIMITED_minutes_4-35_odd_1.1-5_insfufficient_10_dif_1_onlyDraws.csv')
+print(df.head(5))
