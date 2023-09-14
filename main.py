b9763f555fc3d1d967105147433e6e08b34b8320
kk <krzysztof.krystek@gmail.com>
2023-09-14 20:52:46 +0200

    load and save model

--- main.py
+++ main.py
@@ -35,7 +35,68 @@ pipeline = pipe(
 
 # Apply pipeline
 df = pipeline(df)
+df.drop(['datetimestamp'], axis=1, inplace=True)
+
 print(df.shape)
 #print(df.head(10))
 
 
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from skopt import BayesSearchCV
+
+X_train, X_test, y_train, y_test = train_test_split(df.drop('zzz_play', axis=1), df['zzz_play'], test_size=0.2,random_state=42)
+
+xgb_model = xgb.XGBClassifier()
+
+param_space = {
+    'learning_rate': (0.01, 1.0, 'log-uniform'),
+    'max_depth': (1, 50),
+    'min_child_weight': (0, 10),
+    'n_estimators': (50, 1000),
+    'subsample': (0.1, 1.0, 'uniform'),
+    'gamma': (0, 10),
+    'colsample_bytree': (0.1, 1.0, 'uniform'),
+    'reg_alpha': (0, 10),
+    'reg_lambda': (0, 10),
+}
+
+opt = BayesSearchCV(
+    xgb_model,
+    param_space,
+    n_iter=2,#964,9824
+    cv=5,
+    n_jobs=-1,
+    verbose=1,
+)
+
+opt.fit(X_train, y_train)
+
+import json
+
+# Save to file
+with open("best_params.json", "w") as f:
+    json.dump(opt.best_params_, f)
+
+
+# Load from file
+with open("best_params.json", "r") as f:
+    loaded_params = json.load(f)
+
+
+# Initialize model with loaded parameters
+loaded_model = xgb.XGBClassifier(**loaded_params)
+
+# Fit the model
+loaded_model.fit(X_train, y_train)
+##################
+
+
+from sklearn.metrics import accuracy_score
+
+# Make predictions on the test set
+y_pred = loaded_model.predict(X_test)
+
+# Calculate and print accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Test Accuracy: {accuracy}")
