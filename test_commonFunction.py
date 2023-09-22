1d961a40edca90abdb950e4036419cd27678bab9
kk <krzysztof.krystek@gmail.com>
2023-09-22 12:10:53 +0200

    added test_dropInsufficient

--- test_commonFunction.py
+++ test_commonFunction.py
@@ -1,7 +1,6 @@
 import unittest
 import pandas as pd
 from commonFunction import removeDotFromColumnNames, dropMinutes, sortByDate, dropNotDraw, oddsFilter, dropInsufficient, dif_threshold, dropUnnecessary
-
 class TestPandasFunctions(unittest.TestCase):
 
     def setUp(self):
@@ -13,11 +12,11 @@ class TestPandasFunctions(unittest.TestCase):
             'datetimestamp': [1631809781000, 1631809782000, 1631809783000, 1631809784000],
             'frameshomeodd': [1.1, 1.6, 1.7, 2.0],
             'framesawayodd': [1.1, 1.6, 1.7, 2.0],
-            'frameshomeshotsOnTarget': [1, 1, 2, 2],
+            'frameshomeshotsOnTarget': [20, 20, 20, 1],
             'frameshomeshotsOffTarget': [1, 1, 2, 2],
             'frameshomeattacks': [1, 1, 2, 2],
             'frameshomedangerousAttacks': [1, 1, 2, 2],
-            'framesawayshotsOnTarget': [1, 1, 2, 2],
+            'framesawayshotsOnTarget': [20, 20, 20, 2],
             'framesawayshotsOffTarget': [1, 1, 2, 2],
             'framesawayattacks': [1, 1, 2, 2],
             'framesawaydangerousAttacks': [1, 1, 2, 2]
@@ -44,6 +43,17 @@ class TestPandasFunctions(unittest.TestCase):
         result = dropUnnecessary(self.df)
         for col in ['framestime', 'frameshomescore', 'framesawayscore', 'datetimestamp']:
             self.assertNotIn(col, result.columns)
+
+    def test_dropInsufficient(self):
+        specific_columnsA = ['frameshomeshotsOnTarget', 'frameshomeshotsOffTarget', 'frameshomeattacks',
+                            'frameshomedangerousAttacks']
+        specific_columnsB = ['framesawayshotsOnTarget', 'framesawayshotsOffTarget',
+                            'framesawayattacks', 'framesawaydangerousAttacks']
+        self.df['sumAstats'] = self.df[specific_columnsA].sum(axis=1)
+        self.df['sumBstats'] = self.df[specific_columnsB].sum(axis=1)
+        result = dropInsufficient(self.df)
+        self.assertNotIn(3, result.index)
+
 
 if __name__ == '__main__':
     unittest.main()
